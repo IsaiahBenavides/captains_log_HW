cf3995b765e03cd77d1306e61e66a92cc0b45428
Isaiah Bens <benavidesisaiah@outlook.com>
2022-11-23 12:42:34 -0500

    create makes new log

--- server.js
+++ server.js
@@ -1,40 +1,54 @@
 // ========== INITIALIZATION ==========
-require("dotenv").config()
-const express = require("express")
+require(`dotenv`).config()
+const express = require(`express`)
 const app = express()
 const PORT = 3000
-const reactViews = require('express-react-views')
+const reactViews = require(`express-react-views`)
 const methodOverride = require(`method-override`)
-const mongoose = require("mongoose")
+const mongoose = require(`mongoose`)
+const bodyParser = require(`body-parser`)
+const Log = require("./models/logs")
 
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  mongoose.connection.once("open", ()=>{
+  mongoose.connection.once(`open`, ()=>{
     console.log(`connected to mongo`)
   })
   
-  app.set("view engine", "jsx")
-  app.engine("jsx", reactViews.createEngine())
+  app.set(`view engine`, `jsx`)
+  app.engine(`jsx`, reactViews.createEngine())
   
 // ========== MIDDLEWARE ==========
 app.use((req, res, next) =>{
     console.log(`I ran for all routes`)
     next()
   })
-  app.use(express.urlencoded({extended:false}));
-  app.use(methodOverride(`_method`))
+app.use(express.urlencoded({extended:false}));
+app.use(methodOverride(`_method`))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
   // I.N.D.U.C.E.S
 // Index, New, Delete, Update, Create, Edit, Show
 
 // ========== INDEX ==========
-
+app.get(`/logs`, (req,res)=>{
+    Log.find({}, (error, allLogs)=>{
+        if(!error){
+            res.status(200).render(`Index`, {
+                logs: allLogs
+            })
+        }else{
+            res.status(400).send(error)
+        }
+    })
+})
 // ========== NEW ==========
-app.get("/new", (req,res)=>{
-    res.render("New")
+app.get(`/new`, (req,res)=>{
+    res.render(`New`)
 })
 
 // ========== DELETE ==========
@@ -42,6 +56,20 @@ app.get("/new", (req,res)=>{
 // ========== UPDATE ==========
 
 // ========== CREATE ==========
+app.post(`/logs`, (req,res)=>{
+    if(req.body.shipIsBroken === `on`){
+        req.body.shipIsBroken = true
+    }else{
+        req.body.shipIsBroken = false
+    }
+    Log.create(req.body, (error, createdLog)=>{
+        if(!error){
+            res.status(200).redirect(`/logs`)
+        }else{
+            res.status(400).send(error)
+        }
+    })
+})
 
 // ========== EDIT ==========
 
